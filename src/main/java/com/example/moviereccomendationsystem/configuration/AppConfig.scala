f4a9b9886bc0890582419c79bfa9ae635a14faef
MuratTheCommander <muratforbusiness58@gmail.com>
2025-09-06 19:59:48 +0300

    removed unnecessary comments

--- src/main/java/com/example/moviereccomendationsystem/configuration/AppConfig.scala
+++ src/main/java/com/example/moviereccomendationsystem/configuration/AppConfig.scala
@@ -1,12 +1,9 @@
-
 package com.example.moviereccomendationsystem.configuration
-
 import org.apache.spark.sql
 import org.apache.spark.sql.SparkSession
 import org.springframework.context.annotation.{Bean, Configuration}
 import org.springframework.web.client.RestTemplate
 import redis.clients.jedis.Jedis
-
 import java.nio.file.Paths
 import scala.collection.mutable
 
@@ -16,18 +13,12 @@ class AppConfig {
 
   @Bean
   def sparkSession(): SparkSession
-  // (SparkSession, sql.DataFrame, sql.DataFrame, sql.DataFrame, sql.DataFrame)
   = {
-
     val sparkSession = SparkSession.builder()
       .appName("Movie Reccomendation System")
       .master("local[*]") // Use all available cores
       .getOrCreate()
-
-
     println("Spark is being initialized")
-
-    //  (sparkSession,moviesDF,ratingsDF,tagsDF,linksDF)
     sparkSession
   }
 
@@ -64,58 +55,24 @@ class AppConfig {
     ratingsDF
   }
 
-  /* @Bean
-  def linksMoviesDFGenerator(): sql.DataFrame = {
-    // Get the DataFrames only once
-    val moviesDF = moviesDFGenerator()
-    val linksDF = linksDFGenerator()
-
-    // Rename movieId columns to avoid ambiguity in the join
-    val moviesDFRenamed = moviesDF.withColumnRenamed("movieId", "movies_movieId")
-    val linksDFRenamed = linksDF.withColumnRenamed("movieId", "links_movieId")
-
-    // Perform the join between the renamed DataFrames on the 'movieId' column
-    val joinedDF = moviesDFRenamed.join(linksDFRenamed, moviesDFRenamed("movies_movieId") === linksDFRenamed("links_movieId"))
-
-    // Optionally create a temp view for the joined DataFrame
-    joinedDF.createTempView("links_movies_df_temp_view")
-    println("linksMoviesDF initialized successfully")
-
-    joinedDF.show(10)
-
-    // Return the joined DataFrame
-    joinedDF
-  }   */
-
   @Bean(name=Array("ratedMoviesSet"))
   def ratedMoviesSet():mutable.HashMap[Long,Double] = {
-
     val ratedMoviesMap = new mutable.HashMap[Long, Double]()
-
     println("ratedMoviesMap is defined")
-
-    // You can add some data if necessary (e.g., some initial values to the map)
-
-    ratedMoviesMap  // Return the map
+    ratedMoviesMap
   }
 
   @Bean(name=Array("likedMoviesSet"))
   def likedMoviesSet():mutable.HashSet[Long] = {
-
     val likedMoviesSet = new mutable.HashSet[Long]()
-
     println("likedMoviesSet is defined")
-
-    // You can add some data if necessary (e.g., some initial values to the map)
-
-    likedMoviesSet  // Return the map
+    likedMoviesSet
   }
 
 
 
   @Bean(name=Array("jedis0"))
   def jedis0:Jedis={
-    //movie datas
     val jedis0 = new Jedis("localhost",6379)
     jedis0.select(0)
     println("jedis0 is set up")
@@ -124,7 +81,6 @@ class AppConfig {
 
   @Bean(name=Array("jedis1"))
   def jedis1:Jedis={
-    //content based filtering results
     val jedis1 = new Jedis("localhost", 6379)
     jedis1.select(1)
     println("jedis1 is set up")
@@ -133,7 +89,6 @@ class AppConfig {
 
   @Bean(name=Array("jedis2"))
   def jedis2:Jedis={
-    //movie vectors
     val jedis2 = new Jedis("localhost",6379)
     jedis2.select(2)
     println("jedis2 is set up")
@@ -142,7 +97,6 @@ class AppConfig {
 
   @Bean(name=Array("jedis3"))
   def jedis3:Jedis={
-    //content based filtering results for each movie
     val jedis3 = new Jedis("localhost",6379)
     jedis3.select(3)
     println("jedis3 is set up")
@@ -156,13 +110,9 @@ class AppConfig {
 
   @Bean(name=Array("contentBasedMoviesSet"))
   def contentBasedMoviesToReccomend():mutable.LinkedHashMap[Long,String]={
-
     val contentBasedMoviesSet =  new mutable.LinkedHashMap[Long,String]()
-
     println("contentBasedMoviesSet is defined")
-
-
-    contentBasedMoviesSet  // Return the map
+    contentBasedMoviesSet
 
   }
 
